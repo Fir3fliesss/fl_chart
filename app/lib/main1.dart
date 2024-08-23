f62c173ad41cf79f24f8fb3253676b805c7bd1e0
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2024-08-23 11:20:35 +0700

    update

--- app/lib/main1.dart
+++ app/lib/main1.dart
@@ -130,6 +130,7 @@ class _FishFinderAppState extends State<FishFinderApp> {
 
   @override
   void dispose() {
+    _bluetoothClassicPlugin.disconnect();
     _timer?.cancel();
     super.dispose();
   }
