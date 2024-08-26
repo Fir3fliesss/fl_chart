b9b63f1ea1c7dacc4d596b74df62bf657ad73774
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2024-08-26 15:59:17 +0700

    migrate from bluetooth_classic plugin, to flutter_bluetooth_classic

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,15 +1,153 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'main1.dart';
+import 'package:flutter_blue_classic/flutter_blue_classic.dart';
+import 'device_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FishFinderApp(title: 'Fishfinder'),
+    return const MaterialApp(home: MainScreen());
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _flutterBlueClassicPlugin = FlutterBlueClassic(usesFineLocation: true);
+
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  StreamSubscription? _adapterStateSubscription;
+
+  final Set<BluetoothDevice> _scanResults = {};
+  StreamSubscription? _scanSubscription;
+
+  bool _isScanning = false;
+  int? _connectingToIndex;
+  StreamSubscription? _scanningStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    BluetoothAdapterState adapterState = _adapterState;
+
+    try {
+      adapterState = await _flutterBlueClassicPlugin.adapterStateNow;
+      _adapterStateSubscription =
+          _flutterBlueClassicPlugin.adapterState.listen((current) {
+        if (mounted) setState(() => _adapterState = current);
+      });
+      _scanSubscription =
+          _flutterBlueClassicPlugin.scanResults.listen((device) {
+        if (mounted) setState(() => _scanResults.add(device));
+      });
+      _scanningStateSubscription =
+          _flutterBlueClassicPlugin.isScanning.listen((isScanning) {
+        if (mounted) setState(() => _isScanning = isScanning);
+      });
+    } catch (e) {
+      if (kDebugMode) print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _adapterState = adapterState;
+    });
+  }
+
+  @override
+  void dispose() {
+    _adapterStateSubscription?.cancel();
+    _scanSubscription?.cancel();
+    _scanningStateSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<BluetoothDevice> scanResults = _scanResults.toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterBlueClassic example app'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text("Bluetooth Adapter state"),
+            subtitle: const Text("Tap to enable"),
+            trailing: Text(_adapterState.name),
+            leading: const Icon(Icons.settings_bluetooth),
+            onTap: () => _flutterBlueClassicPlugin.turnOn(),
+          ),
+          const Divider(),
+          if (scanResults.isEmpty)
+            const Center(child: Text("No devices found yet"))
+          else
+            for (var (index, result) in scanResults.indexed)
+              ListTile(
+                title: Text("${result.name ?? "???"} (${result.address})"),
+                subtitle: Text(
+                    "Bondstate: ${result.bondState.name}, Device type: ${result.type.name}"),
+                trailing: index == _connectingToIndex
+                    ? const CircularProgressIndicator()
+                    : Text("${result.rssi} dBm"),
+                onTap: () async {
+                  BluetoothConnection? connection;
+                  setState(() => _connectingToIndex = index);
+                  try {
+                    connection =
+                        await _flutterBlueClassicPlugin.connect(result.address);
+                    if (!this.context.mounted) return;
+                    if (connection != null && connection.isConnected) {
+                      if (mounted) setState(() => _connectingToIndex = null);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  DeviceScreen(connection: connection!)));
+                    }
+                  } catch (e) {
+                    if (mounted) setState(() => _connectingToIndex = null);
+                    if (kDebugMode) print(e);
+                    connection?.dispose();
+                    ScaffoldMessenger.maybeOf(context)?.showSnackBar(
+                        const SnackBar(
+                            content: Text("Error connecting to device")));
+                  }
+                },
+              )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          if (_isScanning) {
+            _flutterBlueClassicPlugin.stopScan();
+          } else {
+            _scanResults.clear();
+            _flutterBlueClassicPlugin.startScan();
+          }
+        },
+        label: Text(_isScanning ? "Scanning..." : "Start device scan"),
+        icon: Icon(_isScanning ? Icons.bluetooth_searching : Icons.bluetooth),
+      ),
     );
   }
 }
